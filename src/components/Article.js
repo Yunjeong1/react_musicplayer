879d72e2a9ef10d91727677f2b73fe43c5dc9734
Yunjeong <8768moon@naver.com>
2022-04-06 16:26:47 +0900

    구조분해 할당

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-function Article(props) {
+//public폴더까지의 절대 경로값
+const path = process.env.PUBLIC_URL;
+
+function Article({ data }) {
 	// console.log(props);
 	return (
 		<article>
-			<h1>{props.data}</h1>
-			<img src={props.imgSrc} />
+			<h1>{data}</h1>
+			{/* public폴더까지의 절대경로를 불러와서 그 뒤에 props로 전달받은 이미지명을 이용해 이미지소스 url 완성 */}
+			<img src={`${path}/img/${data}.jpg`} alt={'cover art'} />
 		</article>
 	);
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Article from './Article';
 
-//image import
-import pic1 from '../img/Blizzards.jpg';
-import pic2 from '../img/Calm.jpg';
-import pic3 from '../img/Dusty_Road.jpg';
-import pic4 from '../img/Escape.jpg';
-import pic5 from '../img/Payday.jpg';
-import pic6 from '../img/Retreat.jpg';
-import pic7 from '../img/Seasonal.jpg';
-import pic8 from '../img/Vespers.jpg';
-const imgs = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8];
+//hook: 컴포넌트 안쪽에 다양한 기능을 도와주는 모듈
+//useState: 컴포넌트의 랜더링에 관여하는 중요한 정보값을 관리해주는 훅
+
+/*
+구조분해 할당
+비구조화 할당
+destructuring assignment;
+
+//각 배열의 데이터를 구조분해 할당을 이용해서 원하는 변수명으로 한번에 할당
+const fruit = ['apple', 'pear', 'melon'];
+const [a,b,c] = fruit;
+
+const student = {
+	name: '홍길동',
+	age: 20,
+	isFemale: false,
+}
+const {name, age, isFemale} = student;
+
+*/
 
 /*
   단방향 데이터 바인딩
@@ -34,7 +44,7 @@ function Section() {
 		<section>
 			{arr.map((data, idx) => {
 				//부모요소에서 자식 컴포넌트로 prop(이라는 data를)을 통해서 특정 데이터 전달 가능
-				return <Article key={idx} data={data} imgSrc={imgs[idx]} />;
+				return <Article key={idx} data={data} index={idx} />;
 			})}
 		</section>
 	);
