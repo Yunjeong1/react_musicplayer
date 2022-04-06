c19d05f805492c0b43ef657ba0acfd8d00941953
Yunjeong <8768moon@naver.com>
2022-04-06 15:30:07 +0900

    img

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 function Article(props) {
-	console.log(props);
-	return <article>{props.data}</article>;
+	// console.log(props);
+	return (
+		<article>
+			<h1>{props.data}</h1>
+			<img src={props.imgSrc} />
+		</article>
+	);
 }
 
 export default Article;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Article from './Article';
 
+//image import
+import pic1 from '../img/Blizzards.jpg';
+import pic2 from '../img/Calm.jpg';
+import pic3 from '../img/Dusty_Road.jpg';
+import pic4 from '../img/Escape.jpg';
+import pic5 from '../img/Payday.jpg';
+import pic6 from '../img/Retreat.jpg';
+import pic7 from '../img/Seasonal.jpg';
+import pic8 from '../img/Vespers.jpg';
+const imgs = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8];
+
 /*
   단방향 데이터 바인딩
   - 부모컴포넌트에서 자식 컴포넌트로 데이터를 전달 가능
   - 부모에서 자식요소로 데이터를 전달할때는 prop을 통해서 전달
-
 */
 
 function Section() {
@@ -17,13 +27,14 @@ function Section() {
 		'Payday',
 		'Retreat',
 		'Seasonal',
+		'Vespers',
 	];
 
 	return (
 		<section>
 			{arr.map((data, idx) => {
 				//부모요소에서 자식 컴포넌트로 prop(이라는 data를)을 통해서 특정 데이터 전달 가능
-				return <Article key={idx} data={data} />;
+				return <Article key={idx} data={data} imgSrc={imgs[idx]} />;
 			})}
 		</section>
 	);
